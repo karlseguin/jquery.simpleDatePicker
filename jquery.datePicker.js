3444689ceb9a0409f7f9b93a8a38ef3cd09a3879
Karl Seguin <karl@openmymind.net>
2013-01-01 17:45:10 +0800

    fixing #4 - when selected option is specified, the selected date and selected cell are now properly based on the supplied callback

--- jquery.datePicker.js
+++ jquery.datePicker.js
@@ -4,14 +4,14 @@
     var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
     var abbreviations = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
     var daySelector = 'td:not(.m):not(:empty)';
-    
+
     return this.each(function() {
       if (this.datePicker) { return false; }
       var options = $.extend({}, defaults, opts);
       var $input = $(this);
       var $container = currentDate = mode = null;
       var self = {
-        initialize: function() {               
+        initialize: function() {
           $input.click(function() { self.show(); return false; }).keydown(function(e){ if (e.keyCode == 13) { self.entered(); return false; }});
           $(document).keydown(function(e) { if (e.keyCode == 27) { self.hide(); }}).click(self.hide);
           $container = self.initializeContainer().hide()
@@ -27,10 +27,10 @@
         parseDate: function(value) { return new Date(value); },
         show: function() { $container.show(); },
         hide: function() { $container.hide(); },
-        loadPrevious: function() {               
+        loadPrevious: function() {
           $container.empty().append(self.buildMonth(new Date(currentDate.getFullYear(), currentDate.getMonth()-1, 1)));
         },
-        loadNext: function() {          
+        loadNext: function() {
           $container.empty().append(self.buildMonth(new Date(currentDate.getFullYear(), currentDate.getMonth()+1, 1)));
         },
         hover: function() {
@@ -48,15 +48,15 @@
             $cell.addClass('selected');
             var date = new Date(currentDate.getFullYear(), currentDate.getMonth(), $cell.text());
             $input.val(self.format(date)).change();
-            if (options.selected != null) { options.selected(selected); } 
-            self.hide(); 
+            if (options.selected != null) { options.selected(date, $cell); }
+            self.hide();
           }
-        }, 
+        },
         entered: function() {
           var date = self.parseDate($input.val().replace(/^\s*/, '').replace(/\s*$/, ''));
           if (date == null) { return; }
           $container.empty().append(self.buildMonth(date)).find(daySelector).eq(date.getDate()-1).click();
-        },     
+        },
         initializeContainer: function() {
           return $('<div>').addClass('calendar').insertAfter($input);
         },
@@ -82,7 +82,7 @@
           year.colSpan = 4;
           year.className = 'm year';
           year.innerHTML = currentDate.getFullYear();
-          
+
           //could make this static...
           for (var i = 0; i < 3; ++i) {
             var row = table.insertRow(-1);
@@ -100,9 +100,9 @@
           currentDate = first;
           var firstDay = first.getDay();
           var totalDays = last.getDate();
-          var weeks = Math.ceil((totalDays + firstDay) / 7); 
+          var weeks = Math.ceil((totalDays + firstDay) / 7);
           var table = document.createElement('table');
-          
+
           for (var i = 0, count = 1; i < weeks; ++i) {
             var row = table.insertRow(-1);
             for(var j = 0; j < 7; ++j, ++count) {
@@ -112,12 +112,12 @@
               }
             }
           }
-           
+
           var header = table.insertRow(0);
           self.addHeaderCell(header, '<div class="prev">&laquo;</div>', 1);
           self.addHeaderCell(header, '<div class="month">' + months[date.getMonth()] + ' ' + date.getFullYear() + '</div>', 5);
           self.addHeaderCell(header, '<div class="next">&raquo;</div>', 1);
-          
+
           var $table = $(table);
           if (options.minimumDate && options.minimumDate >= first) { $table.find('.prev').hide(); }
           if (options.maximumDate && options.maximumDate <= last) { $table.find('.next').hide(); }
